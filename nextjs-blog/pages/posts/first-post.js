6aaec26453da7e3ac058a45e46e4927dbb16019f
小嶋 優希 <kojima_yuki@waku-2.com>
2021-02-16 11:15:38 +0900

    initial commit

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from "next/Link";
+
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
